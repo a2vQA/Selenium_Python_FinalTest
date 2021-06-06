4f501b0950c1ef4d895a4fe74989ae69067d1c5d
Vladislav <hellzzzr@gmail.com>
2021-06-07 00:09:30 +0300

    PageObjectModel

--- test_main_page.py
+++ test_main_page.py
@@ -6,15 +6,4 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-
-
-
-#
-#
-#     browser.get(link)
-#     go_to_login_page(browser)
-#
-#
-# def go_to_login_page(browser):
-#     login_link = browser.find_element_by_css_selector("#login_link")
-#     login_link.click()
+    page.should_be_login_link()
