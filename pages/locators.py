e23a001a065fd1140e748a05d82d4e70841ce6b4
Vladislav <hellzzzr@gmail.com>
2021-06-08 01:11:55 +0300

    сделал что то на pageobject, даже со всеми условиями

--- pages/locators.py
+++ pages/locators.py
@@ -9,8 +9,10 @@ class LoginPageLocators:
     login_form = (By.CSS_SELECTOR, "#login_form")
     register_form = (By.CSS_SELECTOR, "#register_form")
 
-#
-# class BasketTaskLocators:
-#     basket_btn = (By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')
-#     added_name = (By.CSS_SELECTOR, '#messages>div:nth-child(1)>div>strong')
-#     book_name = (By.CSS_SELECTOR, '#content_inner>article>div.row>div.col-sm-6.product_main>h1')
+
+class BasketTaskLocators:
+    basket_btn = (By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')
+    added_name = (By.CSS_SELECTOR, '#messages>div:nth-child(1)>div>strong')
+    book_name = (By.CSS_SELECTOR, '#content_inner>article>div.row>div.col-sm-6.product_main>h1')
+    book_price = (By.CSS_SELECTOR, '#content_inner>article>div.row>div.col-sm-6.product_main>p.price_color')
+    basket_price = (By.CSS_SELECTOR, '#messages>div.alert.alert-safe.alert-noicon.alert-info.fade.in>div>p:nth-child(1)>strong')
--- pages/product_page.py
+++ pages/product_page.py
@@ -4,13 +4,14 @@ from .base_page import BasePage
 from selenium.common.exceptions import NoAlertPresentException
 import math
 from selenium import webdriver
-# from .locators import BasketTaskLocators
+from .locators import BasketTaskLocators
+
 
 class ProductPage(BasePage):
 
     def popup_alert(self):
         browser = webdriver
-        btn = self.browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket').click()
+        btn = self.browser.find_element(*BasketTaskLocators.basket_btn).click()
 
     def solve_quiz_and_get_code(self):
         WebDriverWait(self.browser, 3).until(EC.alert_is_present())
@@ -28,12 +29,11 @@ class ProductPage(BasePage):
             print("No second alert presented")
 
     def name_check(self):
-        added_name = self.browser.find_element_by_css_selector('#messages>div:nth-child(1)>div>strong').text
-        book_name = self.browser.find_element_by_css_selector('#content_inner>article>div.row>div.col-sm-6'
-                                                              '.product_main>h1').text
+        added_name = self.browser.find_element(*BasketTaskLocators.added_name).text
+        book_name = self.browser.find_element(*BasketTaskLocators.book_name).text
         assert book_name == added_name, "books are different"
 
     def price_check(self):
-        book_price = self.browser.find_element_by_css_selector('#content_inner>article>div.row>div.col-sm-6.product_main>p.price_color').text
-        basket_price = self.browser.find_element_by_css_selector('#messages>div.alert.alert-safe.alert-noicon.alert-info.fade.in>div>p:nth-child(1)>strong').text
+        book_price = self.browser.find_element(*BasketTaskLocators.book_price).text
+        basket_price = self.browser.find_element(*BasketTaskLocators.basket_price).text
         assert book_price == basket_price, "price are different"
